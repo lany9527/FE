d6a74babc42a1a7e31d557345a89592f8612ba1f
lany9527 <lany9527@gmail.com>
2016-06-17 00:27:00 +0800

    底部toolBar优化

--- app/scripts/directives/console_components.js
+++ app/scripts/directives/console_components.js
@@ -11,11 +11,5 @@ angular.module('feApp')
     return {
       templateUrl: 'views/common/console.html',
       restrict: 'EA'
-      //replace: true
-      //template: '<div>qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq</div>'
-      /*restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the consoleComponents directive');
-      }*/
     };
   });
